6151fd354515d73e937483f484e22310cb16815d
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-09-25 23:41:49 -0500

    boton left optimizado

--- js/index.js
+++ js/index.js
@@ -1,24 +1,11 @@
-let left_one = document.querySelectorAll(".left")[0]
-let left_two = document.querySelectorAll(".left")[1]
-
-
-let rigth_one = document.querySelectorAll(".rigth")[0]
-let rigth_two = document.querySelectorAll(".rigth")[1]
+let left = document.querySelectorAll(".left")
+let rigth = document.querySelectorAll(".rigth")
 
 let contentSlider = document.querySelector(".content-slider")
 
-let SliderLast = document.querySelectorAll(".Slider")[1]
-let Sliderfirst = document.querySelectorAll(".Slider")[0]
-let slider = document.querySelectorAll(".Slider")
 
 
-
-console.log(SliderLast)
-
-left_one.addEventListener("click",function(){
-
-
-
+function slider_direcction(){
     contentSlider.style.marginLeft = "-325px"
     contentSlider.style.transition = "all 0.5s"
 
@@ -31,27 +18,11 @@ left_one.addEventListener("click",function(){
         contentSlider.style.marginLeft = "0"
 
     }, 500);
-    
-
-})
-
-left_two.addEventListener("click",function(){
+}
 
 
+left[0].addEventListener("click",slider_direcction)
 
-    contentSlider.style.marginLeft = "-325px"
-    contentSlider.style.transition = "all 0.5s"
+left[1].addEventListener("click",slider_direcction)
 
-    setTimeout(function(){
-        let Sliderfirst = document.querySelectorAll(".Slider")[0]
-
-        contentSlider.insertAdjacentElement("beforeend",Sliderfirst)
-        contentSlider.style.transition = "none"
-
-        contentSlider.style.marginLeft = "0"
-
-    }, 500);
-    
-
-})
 

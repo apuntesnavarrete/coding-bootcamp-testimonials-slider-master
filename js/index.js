cb20e9ea6609d02f7d121a4c8ba1ee178558005c
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-09-27 21:17:39 -0500

    slider movil terminado sin rem y em

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ let contentSlider = document.querySelector(".content-slider")
 let input_left = "-325px"
 let input_rigth = "325px"
 
-
 function slider_direcction(mov){
     contentSlider.style.marginLeft = mov
     contentSlider.style.transition = "all 0.5s"
@@ -17,8 +16,6 @@ function slider_direcction(mov){
         contentSlider.style.marginLeft = "0"
     }, 500);
 }
-
-
 
 left[0].addEventListener("click",function(){
     slider_direcction(input_left)

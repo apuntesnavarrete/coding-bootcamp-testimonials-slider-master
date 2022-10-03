9cf97e60455078ca1eab89a17c294f7ad7540b69
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-03 18:45:17 -0500

    arreglos

--- js/index.js
+++ js/index.js
@@ -1,35 +1,34 @@
-let left = document.querySelectorAll(".left")
-let rigth = document.querySelectorAll(".rigth")
+let left = document.querySelectorAll(".left");
+let rigth = document.querySelectorAll(".rigth");
 
-let contentSlider = document.querySelector(".content-slider")
+let contentSlider = document.querySelector(".content-slider");
 
-let input_left = "-100%"
-let input_rigth = "100%"
+let input_left = "-100%";
+let input_rigth = "100%";
 
-function slider_direcction(mov){
-    contentSlider.style.marginLeft = mov
-    contentSlider.style.transition = "all 0.5s"
-    setTimeout(function(){
-        let Sliderfirst = document.querySelectorAll(".Slider")[0]
-        contentSlider.insertAdjacentElement("beforeend",Sliderfirst)
-        contentSlider.style.transition = "none"
-        contentSlider.style.marginLeft = "0"
-    }, 500);
+function slider_direcction(mov) {
+  contentSlider.style.marginLeft = mov;
+  contentSlider.style.transition = "all 0.5s";
+  setTimeout(function () {
+    let Sliderfirst = document.querySelectorAll(".Slider")[0];
+    contentSlider.insertAdjacentElement("beforeend", Sliderfirst);
+    contentSlider.style.transition = "none";
+    contentSlider.style.marginLeft = "0";
+  }, 500);
 }
 
-left[0].addEventListener("click",function(){
-    slider_direcction(input_left)
-})
+left[0].addEventListener("click", function () {
+  slider_direcction(input_left);
+});
 
-left[1].addEventListener("click",function(){
-    slider_direcction(input_left)
-})
+left[1].addEventListener("click", function () {
+  slider_direcction(input_left);
+});
 
+rigth[0].addEventListener("click", function () {
+  slider_direcction(input_rigth);
+});
 
-rigth[0].addEventListener("click",function(){
-    slider_direcction(input_rigth)
-})
-
-rigth[1].addEventListener("click",function(){
-    slider_direcction(input_rigth)
-})
+rigth[1].addEventListener("click", function () {
+  slider_direcction(input_rigth);
+});
